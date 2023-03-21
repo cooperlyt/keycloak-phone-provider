27d0308a546c460356151cfc4424e220326e258d
LABAT Romain <contact@romainlabat.fr>
2023-03-21 01:04:06 +0100

    fix: Replace StringBuilder by StringBuffer to make it compatible with java 11 -> keycloak 20.0.5
    Signed-off-by: LABAT Romain <contact@romainlabat.fr>

--- keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/authenticators/browser/SmsOtpMfaAuthenticator.java
+++ keycloak-phone-provider/src/main/java/cc/coopersoft/keycloak/phone/authentication/authenticators/browser/SmsOtpMfaAuthenticator.java
@@ -79,7 +79,7 @@ public class SmsOtpMfaAuthenticator implements Authenticator, CredentialValidato
 
   public void addCookie(AuthenticationFlowContext context, String name, String value, String path, String domain, String comment, int maxAge, boolean secure, boolean httpOnly) {
     HttpResponse response = context.getSession().getContext().getContextObject(HttpResponse.class);
-    StringBuilder cookieBuf = new StringBuilder();
+    StringBuffer cookieBuf = new StringBuffer();
     ServerCookie.appendCookieValue(cookieBuf, 1, name, value, path, domain, comment, maxAge, secure, httpOnly, null);
     String cookie = cookieBuf.toString();
     response.getOutputHeaders().add(HttpHeaders.SET_COOKIE, cookie);
