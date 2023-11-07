5f80f4ccfb3d63ddd84f9ac31d9d9f91eb8c40dc
rezatashtboland <rezatashtboland@gmail.com>
2023-11-07 17:41:46 +0330

    add melli provider

---
+++ keycloak-sms-provider-melli/src/main/java/com/behsa/keycloak/phone/providers/sender/MelliMessageSenderServiceProviderFactory.java
@@ -0,0 +1,32 @@
+package com.behsa.keycloak.phone.providers.sender;
+
+import cc.coopersoft.keycloak.phone.providers.spi.MessageSenderService;
+import cc.coopersoft.keycloak.phone.providers.spi.MessageSenderServiceProviderFactory;
+import org.keycloak.Config;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.models.KeycloakSessionFactory;
+
+public class MelliMessageSenderServiceProviderFactory implements MessageSenderServiceProviderFactory {
+
+    @Override
+    public MessageSenderService create(KeycloakSession keycloakSession) {
+        return new MelliSmsSenderService(keycloakSession.getContext().getRealm().getDisplayName());
+    }
+
+    @Override
+    public void init(Config.Scope scope) {
+    }
+
+    @Override
+    public void postInit(KeycloakSessionFactory keycloakSessionFactory) {
+    }
+
+    @Override
+    public void close() {
+    }
+
+    @Override
+    public String getId() {
+        return "dummy";
+    }
+}
---
+++ keycloak-sms-provider-melli/src/main/java/com/behsa/keycloak/phone/providers/sender/MelliSmsSenderService.java
@@ -0,0 +1,32 @@
+package com.behsa.keycloak.phone.providers.sender;
+
+import cc.coopersoft.keycloak.phone.providers.exception.MessageSendException;
+import cc.coopersoft.keycloak.phone.providers.spi.FullSmsSenderAbstractService;
+import org.jboss.logging.Logger;
+
+import java.util.Random;
+
+public class MelliSmsSenderService extends FullSmsSenderAbstractService {
+
+    private static final Logger logger = Logger.getLogger(MelliSmsSenderService.class);
+
+    public MelliSmsSenderService(String realmDisplay) {
+        super(realmDisplay);
+    }
+
+    @Override
+    public void sendMessage(String phoneNumber, String message) throws MessageSendException {
+
+        // here you call the method for sending messages
+        logger.info(String.format("To: %s >>> %s", phoneNumber, message));
+
+        // simulate a failure
+        if (new Random().nextInt(10) % 5 == 0) {
+            throw new MessageSendException(500, "MSG0042", "Insufficient credits to send message");
+        }
+    }
+
+    @Override
+    public void close() {
+    }
+}
